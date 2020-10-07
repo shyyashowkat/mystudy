f69b6603db4f877363a9ea5532e7d7ee3187b4d5
shyya showkat <shyxum96@gmail.com>
2020-10-07 17:36:41 +0530

    dj

---
+++ index.js
@@ -0,0 +1,11 @@
+function bigImg(x) {
+    x.style.height = "100%";
+    x.style.width = "200px";
+    x.style.margin="12.5%";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "50%";
+    x.style.width = "150px";
+    x.style.marginleft="25%";
+  }
